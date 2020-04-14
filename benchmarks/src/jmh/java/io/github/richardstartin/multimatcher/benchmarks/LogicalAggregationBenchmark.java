ace44b9ba85b5351193fe56c6922a0815823cc47
richardstartin <richard@openkappa.co.uk>
2020-04-14 17:06:12 +0100

    more detailed analysis

--- benchmarks/src/jmh/java/io/github/richardstartin/multimatcher/benchmarks/LogicalAggregationBenchmark.java
+++ benchmarks/src/jmh/java/io/github/richardstartin/multimatcher/benchmarks/LogicalAggregationBenchmark.java
@@ -2,45 +2,85 @@ package io.github.richardstartin.multimatcher.benchmarks;
 
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jol.info.ClassData;
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.info.FieldLayout;
+import org.openjdk.jol.info.GraphLayout;
 
-import java.lang.management.ThreadMXBean;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Check if aggregation of an array at an offset vectorises or not
  */
-@State(Scope.Benchmark)
 public class LogicalAggregationBenchmark {
 
 
-    @Param("256")
-    int targetSize;
+    @State(Scope.Benchmark)
+    public static class BaseState {
 
-    @Param("1024")
-    int sourceSize;
+        byte[] gap;
 
-    @Param({"0", "256", "512", "768"})
-    int offset;
+        @Param("256")
+        int targetSize;
 
-    private long[] source;
-    private long[] target;
+        @Param("1024")
+        int sourceSize;
+
+        @Param({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                "10", "11", "12", "13", "14", "15", "16", "17", "18",
+                "19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
+                "29", "30", "31", "32"})
+        int padding;
+
+        long[] source;
+        long[] target;
 
 
-    @Setup(Level.Trial)
-    public void setup() {
-        source = new long[sourceSize];
-        target = new long[targetSize];
-        for (int i = 0; i < source.length; ++i) {
-            source[i] = ThreadLocalRandom.current().nextLong();
+        @Setup(Level.Trial)
+        public void setup() {
+            source = new long[sourceSize];
+            gap = new byte[padding];
+            target = new long[targetSize];
+            fill(source);
+            fill(target);
+            System.out.println(ClassLayout.parseInstance(source).toPrintable());
+            System.out.println(GraphLayout.parseInstance(source).toPrintable());
+            System.out.println(ClassLayout.parseInstance(gap).toPrintable());
+            System.out.println(GraphLayout.parseInstance(gap).toPrintable());
+            System.out.println(ClassLayout.parseInstance(target).toPrintable());
+            System.out.println(GraphLayout.parseInstance(target).toPrintable());
         }
-        for (int i = 0; i < target.length; ++i) {
-            target[i] = ThreadLocalRandom.current().nextLong();
+
+        private static void fill(long[] data) {
+            for (int i = 0; i < data.length; ++i) {
+                data[i] = ThreadLocalRandom.current().nextLong();
+            }
         }
+
     }
 
+    public static class ConstantOffset256State extends BaseState {
+
+        private static final int offset = 256;
+    }
+
+    public static class ConstantOffset0State extends BaseState {
+
+        private static final int offset = 0;
+    }
+
+    public static class DynamicOffsetState extends BaseState {
+        @Param({"0", "256", "512", "768"})
+        int offset;
+    }
+
+
     @Benchmark
-    public void intersection(Blackhole bh) {
-        for (int i = 0; i < target.length; ++i) {
+    public void intersectionWithOffset(DynamicOffsetState state, Blackhole bh) {
+        var target = state.target;
+        var source = state.source;
+        int offset = state.offset;
+        for (int i = 0; i < state.target.length; ++i) {
             target[i] &= source[offset + i];
         }
         bh.consume(target);
@@ -48,8 +88,31 @@ public class LogicalAggregationBenchmark {
 
 
     @Benchmark
-    public void intersectionNoOffset(Blackhole bh) {
-        for (int i = 0; i < target.length; ++i) {
+    public void intersectionWithConstantOffset256(BaseState state, Blackhole bh) {
+        var target = state.target;
+        var source = state.source;
+        for (int i = 0; i < state.target.length; ++i) {
+            target[i] &= source[ConstantOffset256State.offset + i];
+        }
+        bh.consume(target);
+    }
+
+    @Benchmark
+    public void intersectionWithConstantOffset0(BaseState state, Blackhole bh) {
+        var target = state.target;
+        var source = state.source;
+        for (int i = 0; i < state.target.length; ++i) {
+            target[i] &= source[ConstantOffset0State.offset + i];
+        }
+        bh.consume(target);
+    }
+
+
+    @Benchmark
+    public void intersectionNoOffset(BaseState state, Blackhole bh) {
+        var target = state.target;
+        var source = state.source;
+        for (int i = 0; i < state.target.length; ++i) {
             target[i] &= source[i];
         }
         bh.consume(target);

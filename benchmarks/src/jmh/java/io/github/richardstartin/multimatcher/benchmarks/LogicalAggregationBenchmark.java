108d0aec4b64ac5728614313fab50af045cdecc9
richardstartin <richard@openkappa.co.uk>
2020-04-15 10:34:33 +0100

    add more data

--- benchmarks/src/jmh/java/io/github/richardstartin/multimatcher/benchmarks/LogicalAggregationBenchmark.java
+++ benchmarks/src/jmh/java/io/github/richardstartin/multimatcher/benchmarks/LogicalAggregationBenchmark.java
@@ -23,7 +23,7 @@ public class LogicalAggregationBenchmark {
         @Param("1024")
         int sourceSize;
 
-        @Param({"0", "8", "16", "24", "32", "40", "48", "56", "64"})
+        @Param({"0", "8", "16", "24", "32", "40", "48", "56", "64", "72", "80", "88", "96", "104", "112", "120" })
         int padding;
 
         long[] source;
